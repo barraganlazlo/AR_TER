27cb5e70c6b3b58865af9a707f9734fd4cd4ed4e
lazlo barragan <lazlo.barragan@gmail.com>
2020-05-20 06:43:11 +0200

    add face to camera

---
+++ Assets/Scripts/FaceToCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceToCamera : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 UpAxis = Vector3.up;
+        transform.rotation = Quaternion.LookRotation(Vector3.Cross(UpAxis, Vector3.Cross(UpAxis, -Camera.main.transform.forward)), UpAxis);
+        
+    }
+}
